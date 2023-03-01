1567a14c1f33874ff2cc164c1d5b058bc242828e
Rosine22 <r.uwineza@alustudent.com>
2023-03-01 05:44:48 -0800

    yt

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
-
     for i in range(x):
         try:
             if type(my_list[i]) is int and printed != x:
@@ -9,7 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 printed += 1
         except TypeError:
             continue
-
     print()
-
     return printed
