4eb2e7cd1a4ab7e2bf3564d8b8c98351d1b9c6d3
Rosine22 <r.uwineza@alustudent.com>
2023-03-24 04:14:47 -0700

    ww

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" define an empty class."""
+
+
+class BaseGeometry:
+    """ an empty class"""
+
+    def area(self):
+        """Not done."""
+        raise Exception('area() is not implemented')
+Footer
