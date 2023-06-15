d3b0e8505594ccfc76ba883fb00f659eae365064
Rosine22 <r.uwineza@alustudent.com>
2023-06-15 14:31:22 +0000

    Task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,20 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
-});
+function readFile (filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(`An error occurred while reading the file: ${err}`);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+// Check if the file path is provided as an argument
+if (process.argv.length > 2) {
+  const filePath = process.argv[2];
+  readFile(filePath);
+} else {
+  console.log('Please provide the file path as an argument.');
+}
